0519e8fcc738ff2061ec6f9f4d3dd1f4168a4273
KOUSHIK SHAW <kousjikshaw1111@gmail.com>
2024-01-04 10:16:19 +0530

    updated code

---
+++ square-root.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+#include<time.h>
+
+int main()
+{
+    //Taking input of integer from a user 
+    int x;
+    printf("Square root function ");
+    printf("Enter the number you want square root of : ");
+    scanf("%d",&x);
+
+    //Trying to find the logic
+    int root;
+    
+    return 0;
+}
