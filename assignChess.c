db323cacbb1e12cec11d134f89f29b83c7500dd5
KOUSHIK SHAW <kousjikshaw1111@gmail.com>
2024-04-06 11:02:08 +0530

    added chess board queen game code

---
+++ assignChess.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+struct coordinates
+{
+    int x;
+    int y;
+}dimen;
+int main()
+{
+    int q;
+    printf("Enter the dimensions of the board:\n");
+    scanf("%d%d",&dimen.x,&dimen.y);
+    printf("Enter the number of coordinates: ");
+    scanf("%d",&q);
+    struct coordinates c[q];
+    printf("Enter %d coordinates:\n",q);
+    for (int i = 0; i < q; i++)
+    {
+        printf("coordinates : ");
+        scanf("%d%d",&c[i].x,&c[i].y);
+    }
+    for (int i = 0; i < dimen.x; i++)
+    {
+        for (int j = 0; j < dimen.y; j++)
+        {
+            int count=0;
+            for (int k = 0; k < q; k++)
+            {
+                if ((c[k].x==i)&&(c[k].y==j))
+                {
+                    count=1;
+                }
+            }
+            if (count==1)
+            {
+                printf("Q ");
+            }
+            else if (count==0)
+            {
+                printf("* ");
+            }
+            
+            
+        }
+        printf("\n");
+        
+    }
+    
+    return 0;
+}
